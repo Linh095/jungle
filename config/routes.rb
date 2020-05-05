eb1784ebc932cc6c48a3a58694feabf2d75f6a88
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-05-05 11:38:42 -0400

    made routes for about page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+resource :about, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
