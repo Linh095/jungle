325ac0b50821610e535d09ee4e223e15ff7c5d98
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-05-05 01:53:44 -0400

    trying to add an index page for categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_autentication_with name: ENV['USERNAME'], password: ENV['PASSWORD'] 
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products_total = Product.count
+    @categories_total = Category.count
   end
 end
